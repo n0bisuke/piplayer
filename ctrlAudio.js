2dc76704b77d11d6c6bbf81bfd74b795fbd90b85
のびすけ <rsksound@gmail.com>
2017-04-20 13:33:33 +0900

    deploy

--- ctrlAudio.js
+++ ctrlAudio.js
@@ -2,7 +2,7 @@
 
 const execSync = require('child_process').execSync;
 
-module.export = (vol) => {
+module.exports = (vol) => {
     const cmd = `amixer cset numid=1 ${vol}%`;
     const res = execSync(cmd);
     console.log(res);
