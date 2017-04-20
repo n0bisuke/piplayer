c128f8972c77b6c808b2f680ecb0a91247f920b2
のびすけ <rsksound@gmail.com>
2017-04-20 10:47:32 +0900

    list

---
+++ audio/radioList.js
@@ -0,0 +1,13 @@
+//Stream Radio List
+
+'use strict'
+
+module.exports = {
+    music: [
+        `http://gyusyabu.ddo.jp:8100/;stream.mp3`,
+        `./audio/1.mp3`,
+        `http://stream.srg-ssr.ch/m/rsp/mp3_128`,
+        `http://cabhs30.sonixcast.com:9764/;`
+    ],
+    english: {}
+};
